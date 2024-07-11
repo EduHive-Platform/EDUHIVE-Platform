257f00c7c9313a3ac7e91070ac7c7c7ec74042d8
Zihan Zhao <zihanzhao1117@berkeley.edu>
2024-07-11 18:43:09 +0800

    left bar and square

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,7 +5,7 @@ const Posts = () => {
   return (
     <div className="posts">
       <h2>Posts</h2>
-      {/* Add your post content here */}
+      <h1> ###################### Area for posts ########## ##### right bar########</h1>
     </div>
   );
 };
