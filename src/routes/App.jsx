068a43b49b2e404abd4c40ec050ce68fa5b0dccb
cheng-wang2002 <90290249+cheng-wang2002@users.noreply.github.com>
2024-07-17 00:13:00 -0500

    Update App.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,8 +8,7 @@ import EmailVerification from './EmailVerification';
 import StudentPost from './StudentPost';
 import NotFound from './NotFound';
 import Square from './Square';
-import FormSheet from '../components/Form';
-import PostsPage from '../components/PostsPage.jsx'; // Updated to .jsx
+
 
 function App() {
   return (
@@ -22,8 +21,6 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/email-verification" element={<EmailVerification />} />
         <Route path="/studentPost" element={<StudentPost />} />
-        <Route path="/form" element={<FormSheet />} />
-        <Route path="/posts" element={<PostsPage />} /> {/* Use PostsPage here */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
