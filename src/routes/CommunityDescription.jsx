ad7a6cce9db7bdc448e007cce245c2cc2a7be70b
Zihan Zhao <zihanzhao1117@berkeley.edu>
2024-07-20 23:40:24 +0800

    Community Description + jump to subsquare

--- src/routes/CommunityDescription.jsx
+++ src/routes/CommunityDescription.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import { Button } from '../components/styled_components';
 import communities from '../data/Communities';
 import './CommunityDescription.css';  // Assuming styles are here
 
@@ -16,6 +17,10 @@ const CommunityDescription = () => {
     navigate(-1);  // Go back to the previous page
   };
 
+  const handleLogin = () => {
+    navigate('/subSquare');
+  };
+
   return (
     <div className="community-page">
       <div className="image-container2">
@@ -25,6 +30,10 @@ const CommunityDescription = () => {
         <button className="close-button" onClick={handleBack}>×</button>
         <h1>{community.name}</h1>
         <p>{community.description}</p>
+        <div className="func-buttons">
+          <Button onClick={handleLogin}>To the SubSquare</Button>
+        </div>
+        
       </div>
     </div>
   );
